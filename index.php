6a2064e5d9174aabacb4a7f4a11d67aecae38144
cengicilma <ilma.cengic@gmail.com>
2022-01-20 16:59:01 +0100

    small changes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS, PATCH');
-require '../vendor/autoload.php';
+require './vendor/autoload.php';
 
 /*
 Username: 4Z0PvflNWl
@@ -14,10 +14,10 @@ Port: 3306
 Flight::register('db', 'PDO', array('mysql:host=remotemysql.com;dbname=4Z0PvflNWl','4Z0PvflNWl','wTq9yrRwVU'));
 
 Flight::route('GET /users', function (){
-    $users = Flight::db()->query('SELECT * FROM users', PDO::FETCH_ASSOC)->fetchAll();
+    $users = Flight::db()->query('SELECT * FROM user', PDO::FETCH_ASSOC)->fetchAll();
     Flight::json($users);
 });
 
 Flight::start();
-?>
+
 
